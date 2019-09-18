9410a733217bb916084d6065df4a23c1a54e8b63
Noam Aflalo <noamaflalo@gmail.com>
2019-09-19 00:59:46 +0200

    Add files via upload

---
+++ Pixel.java
@@ -0,0 +1,12 @@
+class Pixel{          //This class represents a Pixel. A pixel is composed of 3 colours.
+      int red;
+      int green;
+      int blue;
+      public Pixel(){this(0,0,0);}
+      public Pixel(int pRed,int pGreen, int pBlue){
+        this.red=pRed;
+        this.green=pGreen;
+        this.blue=pBlue;
+
+      }
+}
